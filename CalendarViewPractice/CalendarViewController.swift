b04081ec78b96daf518218e9dc148e1780c5dd23
torinunna <torinunna9588@gmail.com>
2023-09-13 17:03:40 +0900

    add a calendarView to CalendarVC

--- CalendarViewPractice/CalendarViewController.swift
+++ CalendarViewPractice/CalendarViewController.swift
@@ -6,14 +6,50 @@
 //
 
 import UIKit
+import SnapKit
 
 class CalendarViewController: UIViewController {
-
+    lazy var calendarView: UICalendarView = {
+        var view = UICalendarView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.wantsDateDecorations = true
+        return view
+    }()
+    
+    var selectedDate: DateComponents? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpLayout()
+        setCalendar()
+        reloadCalendarView(date: Date())
     }
-
-
+    
+    fileprivate func setCalendar() {
+        let dateSelection = UICalendarSelectionSingleDate(delegate: self)
+        calendarView.selectionBehavior = dateSelection
+    }
+    
+    func reloadCalendarView(date: Date?) {
+        if date == nil { return }
+        let calendar = Calendar.current
+        calendarView.reloadDecorations(forDateComponents: [calendar.dateComponents([.day, .month, .year], from: date!)], animated: true)
+    }
 }
 
+extension CalendarViewController: UICalendarSelectionSingleDateDelegate {
+    func dateSelection(_ selection: UICalendarSelectionSingleDate, didSelectDate dateComponents: DateComponents?) {
+        selection.setSelected(dateComponents, animated: true)
+        selectedDate = dateComponents
+        reloadCalendarView(date: Calendar.current.date(from: dateComponents!))
+    }
+}
+
+private extension CalendarViewController {
+    func setUpLayout() {
+        view.addSubview(calendarView)
+        calendarView.snp.makeConstraints {
+            $0.leading.trailing.top.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+}
